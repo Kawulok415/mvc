f7273cd8b44a04c912345d105f346505c439487d
Wieslaw Kawulok <info@dwstudio.cz>
2021-03-20 21:11:20 +0100

    Přidané soubory do složky model

---
+++ model/sportsModel.php
@@ -0,0 +1,78 @@
+<?php
+
+class sportsModel
+{
+  function __construct($consetup)
+  {
+    $this->host = $consetup->host;
+    $this->user = $consetup->user;
+    $this->pass = $consetup->pass;
+    $this->db = $consetup->db;
+  }
+
+  public function open_db()
+  {
+    $this->condb=new mysqli($this->host,$this->user,$this->pass,$this->db);
+    if ($this->condb->connect_error)
+    {
+      die("Error in connection: " . $this->condb->connect_error);
+    }
+  }
+
+  public function close_db()
+  {
+    $this->condb->close();
+  }
+
+  public function insertRecord($obj)
+  {
+    try
+    {
+        $this->open_db();
+        $query=$this->condb->prepare("INSERT INTO sports (category,name) VALUES (?, ?)");
+        $query->bind_param("ss",$obj->category,$obj->name);
+        $query->execute;
+        $res= $query->get_result();
+        $last_id=$this->condb->insert_id;
+        $query->close();
+        $this->close_db();
+        return $last_id;
+    }
+    catch (Exception $e)
+    {
+        $this->close_db();
+        throw new $e;
+
+    }
+
+  }
+
+  public function selectRecord($id)
+  {
+    try
+    {
+      $this->open_db();
+      if($id>0)
+      {
+        $query=$this->condb->prepare("SELECT * FROM sports WHERE id=?");
+        $query->bind_param("i",$id);
+      }
+      else {
+        $query=$this->condb->prepare("SELECT * FROM sports");
+      }
+
+      $query->execute;
+      $res= $query->get_result();
+      $query->close();
+      $this->close_db();
+      return $res;
+    }
+    catch (Exception $e)
+    {
+        $this->close_db();
+        throw new $e;
+
+    }
+  }
+}
+ ?>
